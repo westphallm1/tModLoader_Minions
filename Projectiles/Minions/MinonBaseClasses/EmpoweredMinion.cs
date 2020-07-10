59b1daa95d3e6b6d13414a80a3d89d43b48fdbae
= <=>
2020-07-09 20:01:33 -0500

    add base class for empowered minion

---
+++ Projectiles/Minions/MinonBaseClasses/EmpoweredMinion.cs
@@ -0,0 +1,129 @@
+﻿using log4net.Repository.Hierarchy;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace DemoMod.Projectiles.Minions.MinonBaseClasses
+{
+    public abstract class EmpoweredMinionItem<TBuff, TMinion> : MinionItem<TBuff, TMinion> where TBuff: ModBuff where TMinion: EmpoweredMinion<TBuff>
+    {
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if(player.ownedProjectileCounts[item.shoot] == 0)
+            {
+                player.AddBuff(BuffType<TBuff>(), 2);
+                Projectile.NewProjectile(position - new Vector2(5, 0), new Vector2(speedX, speedY), item.shoot, damage, knockBack, Main.myPlayer);
+            } else
+            {
+                for (int i = 0; i < Main.maxProjectiles; i++) {
+                    Projectile other = Main.projectile[i];
+                    if (other.active && other.owner == Main.myPlayer && other.type == item.shoot && other.minionSlots < player.maxMinions)
+                    {
+                        other.ai[0] = 1;
+                        break;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+
+    public abstract class EmpoweredMinion<T> : SimpleMinion<T> where T: ModBuff
+    {
+        protected abstract int ComputeDamage();
+        protected abstract float ComputeSearchDistance();
+        protected abstract float ComputeInertia();
+        protected abstract float ComputeTargetedSpeed();
+        protected abstract float ComputeIdleSpeed();
+
+        protected abstract void SetMinAndMaxFrames(ref int minFrame, ref int maxFrame);
+
+        public override Vector2 IdleBehavior()
+        {
+            if(projectile.ai[0] == 1 && projectile.minionSlots < player.maxMinions)
+            {
+                projectile.minionSlots += 1;
+                projectile.ai[0] = 0;
+            }
+            projectile.damage = ComputeDamage();
+            Vector2 idlePosition = player.Top;
+            idlePosition.X += 48 * -player.direction;
+            idlePosition.Y += -32;
+            Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
+            TeleportToPlayer(vectorToIdlePosition, 2000f);
+            return vectorToIdlePosition;
+        }
+
+        public override Vector2? FindTarget()
+        {
+            float searchDistance = ComputeSearchDistance();
+            if (PlayerTargetPosition(searchDistance, player.Center) is Vector2 target)
+            {
+                return target - projectile.Center;
+            }
+            else if (ClosestEnemyInRange(searchDistance, player.Center) is Vector2 target2)
+            {
+                return target2 - projectile.Center;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public override void TargetedMovement(Vector2 vectorToTargetPosition)
+        {
+            float inertia = ComputeInertia();
+            float speed = ComputeTargetedSpeed();
+            vectorToTargetPosition.Normalize();
+            vectorToTargetPosition *= speed;
+            projectile.velocity = (projectile.velocity * (inertia - 1) + vectorToTargetPosition) / inertia;
+        }
+
+
+        public override void IdleMovement(Vector2 vectorToIdlePosition)
+        {
+            // alway clamp to the idle position
+            float inertia = ComputeInertia();
+            float maxSpeed = ComputeIdleSpeed();
+            Vector2 speedChange = vectorToIdlePosition - projectile.velocity;
+            if(speedChange.Length() > maxSpeed)
+            {
+                speedChange.Normalize();
+                speedChange *= maxSpeed;
+            }
+            projectile.velocity = (projectile.velocity * (inertia - 1) + speedChange) / inertia;
+        }
+
+
+        public override void Animate(int minFrame = 0, int? maxFrame = null)
+        {
+            int max = 0;
+            SetMinAndMaxFrames(ref minFrame, ref max);
+            maxFrame = max;
+			int frameSpeed = 15;
+			projectile.frameCounter++;
+			if (projectile.frameCounter >= frameSpeed) {
+				projectile.frameCounter = 0;
+				projectile.frame++;
+				if (projectile.frame >= (maxFrame ?? Main.projFrames[projectile.type]) ||
+                    projectile.frame < minFrame ||
+                    Math.Abs(projectile.velocity.X) < 2) {
+					projectile.frame = minFrame;
+				}
+			}
+            if(Math.Abs(projectile.velocity.X) > 2)
+            {
+                projectile.spriteDirection = projectile.velocity.X > 0 ? -1 : 1;
+            }
+            projectile.rotation = projectile.velocity.X * 0.05f;
+        }
+    }
+}
--- Projectiles/Minions/TurtleDrakeHatchling/TurtleDrakeHatchling.cs
+++ Projectiles/Minions/TurtleDrakeHatchling/TurtleDrakeHatchling.cs
@@ -1,4 +1,6 @@
-﻿using log4net.Repository.Hierarchy;
+﻿using DemoMod.Projectiles.Minions.MinonBaseClasses;
+using log4net.Repository.Hierarchy;
+using log4net.Util;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -21,7 +23,7 @@ namespace DemoMod.Projectiles.Minions.TurtleDrakeHatchling
         }
     }
 
-    public class TurtleDrakeHatchlingMinionItem: MinionItem<TurtleDrakeHatchlingMinionBuff, TurtleDrakeHatchlingMinion>
+    public class TurtleDrakeHatchlingMinionItem: EmpoweredMinionItem<TurtleDrakeHatchlingMinionBuff, TurtleDrakeHatchlingMinion>
     {
 		public override void SetStaticDefaults() {
 			base.SetStaticDefaults();
@@ -31,7 +33,6 @@ namespace DemoMod.Projectiles.Minions.TurtleDrakeHatchling
 
 		public override void SetDefaults() {
 			base.SetDefaults();
-			item.damage = 20;
 			item.knockBack = 0.5f;
 			item.mana = 10;
 			item.width = 32;
@@ -39,31 +40,10 @@ namespace DemoMod.Projectiles.Minions.TurtleDrakeHatchling
 			item.value = Item.buyPrice(0, 30, 0, 0);
 			item.rare = ItemRarityID.Blue;
 		}
-
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            if(player.ownedProjectileCounts[item.shoot] == 0)
-            {
-                player.AddBuff(BuffType<TurtleDrakeHatchlingMinionBuff>(), 2);
-                Projectile.NewProjectile(position - new Vector2(5, 0), new Vector2(speedX, speedY), item.shoot, damage, knockBack, Main.myPlayer);
-            } else
-            {
-                for (int i = 0; i < Main.maxProjectiles; i++) {
-                    // Fix overlap with other minions
-                    Projectile other = Main.projectile[i];
-                    if (other.active && other.owner == Main.myPlayer && other.type == item.shoot && other.minionSlots < player.maxMinions)
-                    {
-                        other.ai[0] = 1;
-                        break;
-                    }
-                }
-            }
-            return false;
-        }
     }
 
 
-    public class TurtleDrakeHatchlingMinion : SimpleMinion<TurtleDrakeHatchlingMinionBuff>
+    public class TurtleDrakeHatchlingMinion : EmpoweredMinion<TurtleDrakeHatchlingMinionBuff>
     {
 
 		public override void SetStaticDefaults() {
@@ -87,95 +67,55 @@ namespace DemoMod.Projectiles.Minions.TurtleDrakeHatchling
             // TODO: don't count the balloon for collisions
             return base.Colliding(projHitbox, targetHitbox);
         }
-        public override Vector2 IdleBehavior()
-        {
-            Main.NewText(projectile.minionSlots+ " " + player.maxMinions);
-            if(projectile.ai[0] == 1 && projectile.minionSlots < player.maxMinions)
-            {
-                projectile.minionSlots += 1;
-                projectile.ai[0] = 0;
-            }
-            projectile.damage = 8 + 12 * (int)projectile.minionSlots;
-            Vector2 idlePosition = player.Top;
-            idlePosition.X += 48 * -player.direction;
-            idlePosition.Y += -32;
-            Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
-            TeleportToPlayer(vectorToIdlePosition, 2000f);
-            return vectorToIdlePosition;
-        }
-
-        public override Vector2? FindTarget()
-        {
-            float searchDistance = 400f + 50 * projectile.minionSlots;
-            if (PlayerTargetPosition(searchDistance, player.Center) is Vector2 target)
-            {
-                return target - projectile.Center;
-            }
-            else if (ClosestEnemyInRange(searchDistance, player.Center) is Vector2 target2)
-            {
-                return target2 - projectile.Center;
-            }
-            else
-            {
-                return null;
-            }
-        }
 
         public override void TargetedMovement(Vector2 vectorToTargetPosition)
         {
             vectorToTargetPosition.Y += -32; // hit with the body instead of the balloon
-            float inertia = Math.Max(1, 40 - 4 * projectile.minionSlots);
-            float speed = 4 * projectile.minionSlots;
-            vectorToTargetPosition.Normalize();
-            vectorToTargetPosition *= speed;
-            projectile.velocity = (projectile.velocity * (inertia - 1) + vectorToTargetPosition) / inertia;
+            base.TargetedMovement(vectorToTargetPosition);
         }
 
-        public override void IdleMovement(Vector2 vectorToIdlePosition)
+        protected override int ComputeDamage()
         {
-            // alway clamp to the idle position
-            float inertia = Math.Max(1, 40 - 4 * projectile.minionSlots);
-            float maxSpeed = 2 + 4 * projectile.minionSlots;
-            Vector2 speedChange = vectorToIdlePosition - projectile.velocity;
-            if(speedChange.Length() > maxSpeed)
-            {
-                speedChange.Normalize();
-                speedChange *= maxSpeed;
-            }
-            projectile.velocity = (projectile.velocity * (inertia - 1) + speedChange) / inertia;
+            return 8 + 12 * (int)projectile.minionSlots;
         }
 
-        public override void Animate(int minFrame = 0, int? maxFrame = null)
+        protected override float ComputeSearchDistance()
         {
-            if(projectile.minionSlots == 1)
+            return 400 + 50 * projectile.minionSlots;
+        }
+
+        protected override float ComputeInertia()
+        {
+            return Math.Max(10, 40 - 8 * projectile.minionSlots);
+        }
+
+        protected override float ComputeTargetedSpeed()
+        {
+            return 4 * projectile.minionSlots - 1;
+        }
+
+        protected override float ComputeIdleSpeed()
+        {
+            return ComputeTargetedSpeed() + 2;
+        }
+
+        protected override void SetMinAndMaxFrames(ref int minFrame, ref int maxFrame)
+        {
+            switch(projectile.minionSlots)
             {
-                minFrame = 0;
-                maxFrame = 2;
-            } else if (projectile.minionSlots == 2)
-            {
-                minFrame = 2;
-                maxFrame = 4;
-            } else
-            {
-                minFrame = 4;
-                maxFrame = 6;
+                case 1:
+                    minFrame = 0;
+                    maxFrame = 2;
+                    break;
+                case 2:
+                    minFrame = 2;
+                    maxFrame = 4;
+                    break;
+                default:
+                    minFrame = 4;
+                    maxFrame = 6;
+                    break;
             }
-			int frameSpeed = 15;
-			projectile.frameCounter++;
-			if (projectile.frameCounter >= frameSpeed) {
-				projectile.frameCounter = 0;
-				projectile.frame++;
-				if (projectile.frame >= (maxFrame ?? Main.projFrames[projectile.type]) ||
-                    projectile.frame < minFrame ||
-                    Math.Abs(projectile.velocity.X) < 2) {
-					projectile.frame = minFrame;
-				}
-			}
-            if(Math.Abs(projectile.velocity.X) > 2)
-            {
-                projectile.spriteDirection = projectile.velocity.X > 0 ? -1 : 1;
-            }
-            projectile.rotation = projectile.velocity.X * 0.05f;
         }
     }
 }
