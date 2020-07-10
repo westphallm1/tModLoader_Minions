a8c69ac8b477070658e224433722b90346b15a1c
= <=>
2020-07-09 23:59:12 -0500

    add worm length and position calculation code

---
+++ Projectiles/Minions/CircularVectorQueue.cs
@@ -0,0 +1,164 @@
+﻿using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace DemoMod.Projectiles.Minions
+{
+    public class CircularVectorQueue
+    {
+        protected int queueSize;
+        protected float[] backingArray;
+        protected int startingPosition;
+        protected readonly int headerSize;
+        public int Length = 0;
+        private int headPosition
+        {
+            get { return (int)backingArray[startingPosition]; }
+            set
+            {
+                backingArray[startingPosition] = value;
+            }
+        }
+
+        private int tailPosition
+        {
+            get { return (int)backingArray[startingPosition + 1]; }
+            set { backingArray[startingPosition+1] = value; }
+        }
+
+        private int endOfQueue
+        {
+            get { return startOfQueue + 2 * queueSize; }
+        }
+
+        private int startOfQueue
+        {
+            get { return startingPosition + headerSize; }
+        }
+
+        private int NextHeadPosition()
+        {
+            if (headPosition + 2 < endOfQueue)
+            {
+                return headPosition + 2;
+            } else
+            {
+                return startOfQueue;
+            }
+        }
+        private int PreviousHeadPosition()
+        {
+            if (headPosition - 2 >= startOfQueue)
+            {
+                return headPosition - 2;
+            } else
+            {
+                return endOfQueue - 2;
+            }
+        }
+
+        public Vector2 SeekBackwards(int index)
+        {
+            int headIndex;
+            if(headPosition - 2 * index >= startOfQueue)
+            {
+                headIndex = headPosition - 2 * index;
+            } else
+            {
+                int distancePastStart = startOfQueue - (headPosition - 2 * index);
+                headIndex = endOfQueue + distancePastStart;
+            }
+            return new Vector2(backingArray[headIndex], backingArray[headIndex + 1]);
+        }
+
+        private int NextTailPosition()
+        {
+            if (tailPosition + 2 < endOfQueue)
+            {
+                return tailPosition + 2;
+            } else
+            {
+                return startOfQueue;
+            }
+        }
+        public void Enqueue(Vector2 position)
+        {
+            if(IsFull())
+            {
+                throw new IndexOutOfRangeException("Queue is full!");
+            }
+            Length += 1;
+            backingArray[headPosition] = position.X;
+            backingArray[headPosition + 1] = position.Y;
+            headPosition = NextHeadPosition();
+        }
+
+        public Vector2 Peek()
+        {
+            if(IsEmpty())
+            {
+                throw new IndexOutOfRangeException("Queue is full!");
+            }
+            int prevHead = PreviousHeadPosition();
+            return new Vector2(backingArray[prevHead], backingArray[prevHead + 1]);
+        }
+        public void PeekWrite(Vector2 toWrite)
+        {
+            if(IsEmpty())
+            {
+                throw new IndexOutOfRangeException("Queue is full!");
+            }
+            int prevHead = PreviousHeadPosition();
+            backingArray[prevHead] = toWrite.X;
+            backingArray[prevHead + 1] = toWrite.Y;
+        }
+
+        public Vector2 Dequeue()
+        {
+            if(IsEmpty())
+            {
+                throw new IndexOutOfRangeException("Queue is full!");
+            }
+            Length -= 1;
+            Vector2 dequeued = new Vector2(backingArray[tailPosition], backingArray[tailPosition + 1]);
+            tailPosition = NextTailPosition();
+            return dequeued;
+
+        }
+
+        public bool IsFull ()
+        {
+            return NextHeadPosition() == tailPosition;
+        }
+
+        public bool IsEmpty ()
+        {
+            return headPosition == tailPosition;
+        }
+
+        public CircularVectorQueue(float[] backing, int startingPosition = 0, int headerSize = 2, int queueSize = 16)
+        {
+            backingArray = backing ?? new float[queueSize];
+            this.startingPosition = startingPosition;
+            if(startingPosition + 2 * queueSize > backing.Length)
+            {
+                throw new IndexOutOfRangeException("Backing array not sufficient to hold queue!");
+            }
+            this.queueSize = queueSize;
+            this.headerSize = headerSize;
+        }
+
+        public static void Initialize(float[] backing, int startingPosition = 0, int headerSize = 2)
+        {
+            var queue = new CircularVectorQueue(backing, startingPosition, headerSize);
+            queue.headPosition = queue.startOfQueue;
+            queue.tailPosition = queue.startOfQueue;
+        }
+    }
+}
---
+++ Projectiles/Minions/FlyingSword/FlyingSword.cs
@@ -0,0 +1,147 @@
+﻿using log4net.Repository.Hierarchy;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace DemoMod.Projectiles.Minions.FlyingSword
+{
+    public class FlyingSwordMinionBuff: MinionBuff
+    {
+        public FlyingSwordMinionBuff() : base(ProjectileType<FlyingSwordMinion>(), ProjectileType<FlyingSwordMinion>()) { }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+			DisplayName.SetDefault("Flying Sword");
+			Description.SetDefault("A possessed copper sword will fight for you!");
+        }
+    }
+
+    public class FlyingSwordMinionItem: MinionItem<FlyingSwordMinionBuff, FlyingSwordMinion>
+    {
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Flying Sword");
+			Tooltip.SetDefault("Summons a possessed Sword to fight for you!");
+		}
+
+		public override void SetDefaults() {
+			base.SetDefaults();
+			item.damage = 42;
+			item.knockBack = 0.5f;
+			item.mana = 10;
+			item.width = 32;
+			item.height = 32;
+			item.value = Item.buyPrice(0, 30, 0, 0);
+			item.rare = ItemRarityID.Blue;
+		}
+    }
+    public class FlyingSwordMinion : GroupAwareMinion<FlyingSwordMinionBuff>
+    {
+
+        int hitCount = 0;
+        int maxHitCount = 8;
+        int framesInAir = 0;
+        int maxFramesInAir = 180;
+
+
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Flying Sword");
+			// Sets the amount of frames this minion has on its spritesheet
+			Main.projFrames[projectile.type] = 4;
+		}
+
+		public sealed override void SetDefaults() {
+			base.SetDefaults();
+			projectile.width = 16;
+			projectile.height = 16;
+			projectile.tileCollide = false;
+            projectile.type = ProjectileType<FlyingSwordMinion>();
+            projectile.ai[0] = 0;
+            attackState = AttackState.IDLE;
+            attackFrames = 60;
+		}
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if(hitCount++ >= maxHitCount)
+            {
+                attackState = AttackState.RETURNING;
+            }
+            Dust.NewDust(projectile.Center, projectile.width / 2, projectile.height / 2, DustID.Platinum);
+        }
+
+        public override Vector2 IdleBehavior()
+        {
+            base.IdleBehavior();
+            List<Projectile> minions = GetActiveMinions();
+            int minionCount = minions.Count;
+            int order = minions.IndexOf(projectile);
+            float idleAngle = (float)(2 * Math.PI * order) / minionCount;
+            idleAngle += (2 * (float)Math.PI * minions[0].ai[1]) / animationFrames;
+            Vector2 idlePosition = player.Center;
+            idlePosition.X += (30 + projectile.minionPos * 20) * -player.direction;
+            idlePosition.Y += -35 + 5 * (float) Math.Sin(idleAngle);
+            Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
+            TeleportToPlayer(vectorToIdlePosition, 2000f);
+            return vectorToIdlePosition;
+        }
+
+        public override Vector2? FindTarget()
+        {
+            if(FindTargetInTurnOrder(900f, player.Center) is Vector2 target)
+            {
+                projectile.friendly = true;
+                return target;
+            } else
+            {
+                projectile.friendly = false;
+                return null;
+            }
+        }
+
+        public override void TargetedMovement(Vector2 vectorToTargetPosition)
+        {
+            // alway clamp to the idle position
+            int inertia = hitCount == 0? 5 : 30;
+            int speed = 12;
+            vectorToTargetPosition.Normalize();
+            vectorToTargetPosition *= speed;
+            projectile.velocity = (projectile.velocity * (inertia - 1) + vectorToTargetPosition) / inertia;
+            projectile.rotation += (float)Math.PI / 9;
+            if(framesInAir ++ >= maxFramesInAir)
+            {
+                attackState = AttackState.RETURNING;
+            }
+            Lighting.AddLight(projectile.position, Color.LightGray.ToVector3() * 0.5f);
+        }
+
+        public override void IdleMovement(Vector2 vectorToIdlePosition)
+        {
+            // alway clamp to the idle position
+            int inertia = 5;
+            int maxSpeed = 20;
+            if(vectorToIdlePosition.Length() < 32)
+            {
+                // return to the attacking state after getting back home
+                attackState = AttackState.IDLE;
+                hitCount = 0;
+                framesInAir = 0;
+            }
+            Vector2 speedChange = vectorToIdlePosition - projectile.velocity;
+            if(speedChange.Length() > maxSpeed)
+            {
+                speedChange.Normalize();
+                speedChange *= maxSpeed;
+            }
+            projectile.velocity = (projectile.velocity * (inertia - 1) + speedChange) / inertia;
+
+			float intendedRotation = projectile.velocity.X * 0.05f;
+            projectile.rotation = intendedRotation;
+        }
+    }
+}
