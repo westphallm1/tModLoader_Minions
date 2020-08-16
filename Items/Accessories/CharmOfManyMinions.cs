6bec9398e3b81a753b272c4a8d7feda8fa0323e7
= <=>
2020-08-16 13:08:36 -0500

    add two early hardmode summoner accessories

---
+++ Items/Accessories/CharmOfManyMinions.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+using DemoMod.Projectiles.Minions.VoidKnife;
+
+namespace DemoMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Neck)]
+    class CharmOfManyMinions : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Increases max number of minions by 1,\n" +
+                "but each minion deals slightly less damage");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 32;
+            item.accessory = true;
+            item.value = Item.sellPrice(gold: 5);
+            item.rare = ItemRarityID.LightRed;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.minionDamageMult -= 0.1f;
+            player.maxMinions += 1;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.LightShard, 1);
+            recipe.AddIngredient(ItemID.SoulofNight, 8);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
--- Items/Accessories/CharmOfMightyMinions.cs
+++ Items/Accessories/CharmOfMightyMinions.cs
@@ -3,20 +3,17 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
-using DemoMod.Projectiles.Minions.PossessedCopperSword;
-using DemoMod.Projectiles.Minions.SpiritGun;
-using DemoMod.Projectiles.Minions.FlyingSword;
 using DemoMod.Projectiles.Minions.VoidKnife;
 
 namespace DemoMod.Items.Accessories
 {
     [AutoloadEquip(EquipType.Neck)]
-    class AmuletOfManyMinions : ModItem
+    class CharmOfMightyMinions : ModItem
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("Increases your max number of minions by 2\n" +
-                "Greatly improves minion damage");
+            Tooltip.SetDefault("Greatly increases minion damage,\n" +
+                "but reduces max number of minions by 1");
         }
 
         public override void SetDefaults()
@@ -25,24 +22,20 @@ namespace DemoMod.Items.Accessories
             item.height = 32;
             item.accessory = true;
             item.value = Item.sellPrice(gold: 5);
-            item.rare = ItemRarityID.Expert;
+            item.rare = ItemRarityID.LightRed;
         }
 
         public override void UpdateEquip(Player player)
         {
             player.minionDamageMult += 0.25f;
-            player.maxMinions += 2;
+            player.maxMinions -= 1;
         }
 
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemType<CopperSwordMinionItem>(), 1);
-            recipe.AddIngredient(ItemType<SpiritGunMinionItem>(), 1);
-            recipe.AddIngredient(ItemType<FlyingSwordMinionItem>(), 1);
-            recipe.AddIngredient(ItemType<VoidKnifeMinionItem>(), 1);
-            recipe.AddIngredient(ItemType<CharmOfManyMinions>(), 1);
-            recipe.AddIngredient(ItemType<CharmOfMightyMinions>(), 1);
+            recipe.AddIngredient(ItemID.DarkShard, 1);
+            recipe.AddIngredient(ItemID.SoulofLight, 8);
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
