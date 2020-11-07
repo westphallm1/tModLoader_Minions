24ac1da8afb0a9b7a5c2c9209706534922c24c49
direwolf420 <amakejk1@web.de>
2020-11-07 12:16:34 +0100

    items drop with prefix possibility

--- NPCs/LootTable.cs
+++ NPCs/LootTable.cs
@@ -24,16 +24,16 @@ namespace AmuletOfManyMinions.NPCs
 
 			if (spawnChance < 0.08f && npc.type == NPCID.ManEater)
 			{
-				Item.NewItem(npc.getRect(), ItemType<AncientCobaltSquireMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<AncientCobaltSquireMinionItem>(), 1, prefixGiven: -1);
 			}
-			if (spawnChance < 0.03f && npc.TypeName == "Hornet")
+			if (spawnChance < 0.03f && npc.TypeName == "Hornet") //hacky, should use a custom set and check npc.netID
 			{
-				Item.NewItem(npc.getRect(), ItemType<AncientCobaltSquireMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<AncientCobaltSquireMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.33f && npc.type == NPCID.QueenBee)
 			{
-				Item.NewItem(npc.getRect(), ItemType<BeeQueenMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<BeeQueenMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.01f && (npc.type == NPCID.AngryBones ||
@@ -41,27 +41,27 @@ namespace AmuletOfManyMinions.NPCs
 			   npc.type == NPCID.AngryBonesBigHelmet ||
 			   npc.type == NPCID.AngryBonesBigMuscle))
 			{
-				Item.NewItem(npc.getRect(), ItemType<BoneSquireMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<BoneSquireMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.25f && npc.type == NPCID.WallofFlesh)
 			{
-				Item.NewItem(npc.getRect(), ItemType<BoneSerpentMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<BoneSerpentMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.33f && npc.type == NPCID.GoblinSummoner)
 			{
-				Item.NewItem(npc.getRect(), ItemType<GoblinGunnerMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<GoblinGunnerMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.10f && npc.type == NPCID.Eyezor)
 			{
-				Item.NewItem(npc.getRect(), ItemType<SqueyereMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<SqueyereMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.33f && npc.type == NPCID.Plantera)
 			{
-				Item.NewItem(npc.getRect(), ItemType<PottedPalMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<PottedPalMinionItem>(), 1, prefixGiven: -1);
 			}
 
 			if (spawnChance < 0.03f && (npc.type == NPCID.BlueArmoredBones ||
@@ -69,12 +69,12 @@ namespace AmuletOfManyMinions.NPCs
 			   npc.type == NPCID.BlueArmoredBonesNoPants ||
 			   npc.type == NPCID.BlueArmoredBonesSword))
 			{
-				Item.NewItem(npc.getRect(), ItemType<ArmoredBoneSquireMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<ArmoredBoneSquireMinionItem>(), 1, prefixGiven: -1);
 			}
 
-			if (spawnChance < 0.025f && npc.TypeName == "Hell Armored Bones")
+			if (spawnChance < 0.025f && npc.TypeName == "Hell Armored Bones") //hacky, should use a custom set and check npc.netID
 			{
-				Item.NewItem(npc.getRect(), ItemType<CharredChimeraMinionItem>(), 1);
+				Item.NewItem(npc.getRect(), ItemType<CharredChimeraMinionItem>(), 1, prefixGiven: -1);
 			}
 		}
 
