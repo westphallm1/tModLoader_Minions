621a98cc7016c4de24e67dbe76e732d8a96cdb60
direwolf420 <amakejk1@web.de>
2020-11-09 15:55:18 +0100

    add basic net handler and mouse packet

---
+++ Core/Netcode/Packets/MPPacket.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace AmuletOfManyMinions.Core.Netcode.Packets
+{
+	/// <summary>
+	/// A packet you have to instantiate and send. Classes inheriting from it are autoloaded by NetHandler
+	/// </summary>
+	public abstract class MPPacket
+	{
+		//Shortcut
+		public void Send(int to = -1, int from = -1)
+		{
+			NetHandler.Send(this, to, from);
+		}
+
+		/// <summary>
+		/// Write data to the packet
+		/// </summary>
+		public abstract void Send(BinaryWriter writer, int to = -1, int from = -1);
+
+		/// <summary>
+		/// Any class fields are possibly uninitialized here, they should only be used for Send
+		/// </summary>
+		public abstract void Receive(BinaryReader reader, int sender);
+	}
+}
