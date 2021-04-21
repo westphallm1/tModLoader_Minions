f44e00328c15f5290c1fc7a6701b447f04dd5e20
= <=>
2021-04-21 01:06:44 -0500

    clean up sheep sprite drawing code and add sheep attack

---
+++ Projectiles/Minions/MinonBaseClasses/SpriteCompositionHelper.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace AmuletOfManyMinions.Projectiles.Minions.MinonBaseClasses
+{
+	internal delegate void SpriteCycleDrawer(SpriteCompositionHelper helper, int frame, float angle);
+
+	class SpriteCompositionHelper
+	{
+		private SimpleMinion minion;
+
+		private Projectile projectile => minion.projectile;
+
+		internal int walkCycleFrames = 60;
+		internal int idleCycleFrames = 90;
+		internal int walkCycleFrame = 0;
+		internal int walkVelocityThreshold = 1;
+
+		internal SpriteBatch spriteBatch;
+		internal Color lightColor;
+
+		internal float WalkCycleAngle => Math.Sign(projectile.velocity.X) * MathHelper.TwoPi * (walkCycleFrame % walkCycleFrames) / walkCycleFrames;
+		internal float IdleCycleAngle => MathHelper.TwoPi * (minion.animationFrame % idleCycleFrames) / idleCycleFrames;
+		internal bool IsWalking => Math.Abs(projectile.velocity.X) > walkVelocityThreshold;
+
+		public SpriteCompositionHelper(SimpleMinion minion)
+		{
+			this.minion = minion;
+		}
+
+		public void UpdateMovement()
+		{
+			if(Math.Abs(projectile.velocity.X) > walkVelocityThreshold)
+			{
+				walkCycleFrame++;
+			} else
+			{
+				walkCycleFrame = 0;
+			}
+		}
+
+		public void SetDrawInfo(SpriteBatch spriteBatch, Color lightColor)
+		{
+			this.spriteBatch = spriteBatch;
+			this.lightColor = lightColor;
+		}
+
+		public void ClearDrawInfo()
+		{
+			// don't hang onto reference for too long
+			this.spriteBatch = null;
+		}
+
+		public void AddSpriteToBatch(Texture2D texture, Rectangle bounds, Vector2 offsetFromCenter, float r, float scale)
+		{
+			float frameOfReferenceR = projectile.rotation + r;
+			Vector2 pos = projectile.Center + offsetFromCenter.RotatedBy(frameOfReferenceR);
+			SpriteEffects effects = projectile.spriteDirection == 1 ? 0 : SpriteEffects.FlipHorizontally;
+			Vector2 origin = new Vector2(bounds.Width / 2, bounds.Height / 2);
+			spriteBatch.Draw(texture, pos - Main.screenPosition, bounds, lightColor, frameOfReferenceR, origin, scale, effects, 0);
+		}
+
+		public void AddSpriteToBatch(Texture2D texture, Vector2 offsetFromCenter, float r, float scale)
+		{
+			AddSpriteToBatch(texture, texture.Bounds, offsetFromCenter, r, scale);
+		}
+
+		public void AddSpriteToBatch(Texture2D texture, Rectangle bounds, Vector2 offsetFromCenter)
+		{
+			AddSpriteToBatch(texture, bounds, offsetFromCenter, 0, 1);
+		}
+		public void AddSpriteToBatch(Texture2D texture, Vector2 offsetFromCenter)
+		{
+			AddSpriteToBatch(texture, texture.Bounds, offsetFromCenter, 0, 1);
+		}
+
+		internal void Process(SpriteBatch spriteBatch, Color lightColor, bool isWalking, params SpriteCycleDrawer[] drawers)
+		{
+			SetDrawInfo(spriteBatch, lightColor);
+			if(isWalking)
+			{
+				for(int i = 0; i < drawers.Length; i++)
+				{
+					drawers[i].Invoke(this, walkCycleFrame, WalkCycleAngle);
+				}
+			} else
+			{
+				for(int i = 0; i < drawers.Length; i++)
+				{
+					drawers[i].Invoke(this, minion.animationFrame, IdleCycleAngle);
+				}
+			}
+			ClearDrawInfo();
+		}
+	}
+}
--- Projectiles/Minions/TumbleSheep/TumbleSheep.cs
+++ Projectiles/Minions/TumbleSheep/TumbleSheep.cs
@@ -37,8 +37,8 @@ namespace AmuletOfManyMinions.Projectiles.Minions.TumbleSheep
 		public override void SetDefaults()
 		{
 			base.SetDefaults();
-			item.damage = 10;
-			item.knockBack = 0.5f;
+			item.damage = 11;
+			item.knockBack = 4f;
 			item.mana = 10;
 			item.width = 28;
 			item.height = 28;
@@ -60,10 +60,16 @@ namespace AmuletOfManyMinions.Projectiles.Minions.TumbleSheep
 	public class TumbleSheepMinion : SimpleGroundBasedMinion
 	{
 		protected override int BuffId => BuffType<TumbleSheepMinionBuff>();
-		int lastFiredFrame = 0;
-		int walkCycleFrame = 0;
+		static int bounceCycleLength = 60;
+		int lastFiredFrame = -bounceCycleLength;
 		// don't get too close
-		int preferredDistanceFromTarget = 128;
+		int preferredDistanceFromTarget = 64;
+
+		private Vector2 launchPos;
+
+		private bool IsBouncing => animationFrame - lastFiredFrame < bounceCycleLength;
+
+		private SpriteCompositionHelper scHelper;
 
 		public override void SetStaticDefaults()
 		{
@@ -76,27 +82,20 @@ namespace AmuletOfManyMinions.Projectiles.Minions.TumbleSheep
 			base.SetDefaults();
 			projectile.width = 32;
 			projectile.height = 32;
-			attackFrames = 30;
+			attackFrames = 90;
 			noLOSPursuitTime = 300;
 			startFlyingAtTargetHeight = 96;
 			startFlyingAtTargetDist = 64;
 			defaultJumpVelocity = 4;
 			searchDistance = 900;
 			maxJumpVelocity = 12;
+			scHelper = new SpriteCompositionHelper(this);
 		}
 
-		protected override void IdleFlyingMovement(Vector2 vector)
+		public override void OnSpawn()
 		{
-			if(animationFrame - lastFiredFrame < 10)
-			{
-				// don't fly while throwing the spear
-				gHelper.didJustLand = false;
-				gHelper.isFlying = false;
-				gHelper.ApplyGravity();
-			} else
-			{
-				base.IdleFlyingMovement(vector);
-			}
+			base.OnSpawn();
+			projectile.damage = (int)(1.5f * projectile.damage);
 		}
 
 		protected override void DoGroundedMovement(Vector2 vector)
@@ -125,22 +124,68 @@ namespace AmuletOfManyMinions.Projectiles.Minions.TumbleSheep
 			}
 		}
 
+		private void LaunchBounce(Vector2 vectorToTarget)
+		{
+			lastFiredFrame = animationFrame;
+			launchPos = projectile.position;
+			Main.PlaySound(new LegacySoundStyle(2, 17), projectile.position);
+			if(gHelper.didJustLand && vectorToTarget.Y > -Math.Abs(vectorToTarget.X/4))
+			{
+				vectorToTarget.Y = -Math.Abs(vectorToTarget.X / 4);
+			}
+			vectorToTarget.SafeNormalize();
+			vectorToTarget *= 8;
+			projectile.velocity = vectorToTarget;
+			for(int i = 0; i < 3; i++)
+			{
+				int idx = Dust.NewDust(projectile.BottomLeft - new Vector2(0, 16), 32, 16, 16, 0, 0);
+				Main.dust[idx].alpha = 112;
+				Main.dust[idx].scale = 1.2f;
+			}
+			// TODO
+		}
+
 		// If your name is AG...
 		private void DoBounce()
 		{
-			lastFiredFrame = animationFrame;
-			Main.PlaySound(new LegacySoundStyle(2, 17), projectile.position);
-			// TODO
+			if(animationFrame - lastFiredFrame > 10 && projectile.velocity.Y < 16)
+			{
+				projectile.velocity.Y += 0.5f;
+			}
+			if(Vector2.DistanceSquared(launchPos, projectile.position) > 240 * 240)
+			{
+				// snap out of bounce if we go too far in a straight line
+				lastFiredFrame = animationFrame - bounceCycleLength;
+			}
+
+		}
+
+		public override void IdleMovement(Vector2 vectorToIdlePosition)
+		{
+			if(IsBouncing)
+			{
+				DoBounce();
+			} else
+			{
+				base.IdleMovement(vectorToIdlePosition);
+			}
 		}
 
 		public override void TargetedMovement(Vector2 vectorToTargetPosition)
 		{
 
-			if (Math.Abs(vectorToTargetPosition.X) < 4 * preferredDistanceFromTarget &&
-				Math.Abs(vectorToTargetPosition.Y) < 4 * preferredDistanceFromTarget &&
-				animationFrame - lastFiredFrame >= attackFrames)
+			if(IsBouncing)
 			{
 				DoBounce();
+				return;
+			}
+
+			if (Math.Abs(vectorToTargetPosition.X) < 4 * preferredDistanceFromTarget &&
+				Math.Abs(vectorToTargetPosition.Y) < preferredDistanceFromTarget &&
+				animationFrame - lastFiredFrame >= attackFrames)
+			{
+				LaunchBounce(vectorToTargetPosition);
+				return;
 			}
 
 			// don't move if we're in range-ish of the target
@@ -157,82 +202,123 @@ namespace AmuletOfManyMinions.Projectiles.Minions.TumbleSheep
 
 		public override void AfterMoving()
 		{
-			projectile.friendly = false;
+			projectile.friendly = IsBouncing;
+			projectile.tileCollide |= IsBouncing;
+			scHelper.UpdateMovement();
 		}
 
-		private float WalkCycleAngle => Math.Sign(projectile.velocity.X) * MathHelper.TwoPi * (walkCycleFrame % 60) / 60;
-		private bool IsWalking => Math.Abs(projectile.velocity.X) > 1;
-
-		private void DrawLegs(SpriteBatch spriteBatch, Color lightColor)
+		private void DrawLegs(SpriteCompositionHelper helper, int frame, float cycleAngle)
 		{
+			// used for both idle and active
+			if(gHelper.isFlying && !IsBouncing)
+			{
+				return;
+			}
+			cycleAngle = helper.IsWalking ? cycleAngle : 0;
 			for(int i = 0; i < 2; i++)
 			{
-				Vector2 legRotationAngle = (WalkCycleAngle + i * MathHelper.Pi).ToRotationVector2();
+				Vector2 legRotationAngle = (cycleAngle + i * MathHelper.Pi).ToRotationVector2();
 				legRotationAngle.X *= 4;
 				legRotationAngle.Y *= 1.5f;
-				Texture2D texture = GetTexture(Texture + "_Legs" + (i+1));
-				float r = 0;
-				Vector2 pos = projectile.Center + legRotationAngle + new Vector2(0, 12);
-				SpriteEffects effects = projectile.spriteDirection == 1 ? 0 : SpriteEffects.FlipHorizontally;
-				Rectangle bounds = new Rectangle(0, 0, texture.Width, texture.Height);
-				Vector2 origin = new Vector2(bounds.Width / 2, bounds.Height / 2);
-				spriteBatch.Draw(texture, pos - Main.screenPosition, bounds, lightColor, r, origin, 1, effects, 0);
+				helper.AddSpriteToBatch(GetTexture(Texture + "_Legs" + (i+1)), legRotationAngle + new Vector2(0, 12));
 			}
 		}
 
-		private void DrawBody(SpriteBatch spriteBatch, Color lightColor)
+		private void DrawClouds(SpriteCompositionHelper helper, int frame, float cycleAngle)
 		{
-			Texture2D texture = GetTexture(Texture);
-			float r = IsWalking ? -MathHelper.Pi/64 + MathHelper.Pi/32 * (float)Math.Sin(WalkCycleAngle) : 0;
-			float bobAngle = IsWalking ? 2 * WalkCycleAngle : MathHelper.TwoPi * animationFrame/ 90 + MathHelper.Pi/2;
-			float bobAmplitude = IsWalking ? 2 : 1;
-			Vector2 pos = projectile.Center + (-2  + bobAmplitude * (float)Math.Sin(bobAngle)) * Vector2.UnitY;
-			SpriteEffects effects = projectile.spriteDirection == 1 ? 0 : SpriteEffects.FlipHorizontally;
-			Rectangle bounds = new Rectangle(0, 0, texture.Width, texture.Height);
-			Vector2 origin = new Vector2(bounds.Width / 2, bounds.Height / 2);
-			spriteBatch.Draw(texture, pos - Main.screenPosition, bounds, lightColor, r, origin, 1, effects, 0);
+			if(IsBouncing || !gHelper.isFlying)
+			{
+				return;
+			}
+			for(int i = 0; i < 3; i++)
+			{
+				float mySin = (float)Math.Sin(cycleAngle + i * MathHelper.TwoPi / 3);
+				Vector2 offsetVector = new Vector2(12 * (i - 1), 12 + 3 * mySin);
+				float r = -MathHelper.Pi / 64 + MathHelper.Pi / 32 * (float)mySin;
+				byte oldAlpha = helper.lightColor.A;
+				helper.lightColor.A = 128;
+				helper.AddSpriteToBatch(GetTexture(Texture), offsetVector, r, 0.5f);
+				helper.lightColor.A = oldAlpha;
+			}
 		}
 
-		private void DrawHead(SpriteBatch spriteBatch, Color lightColor)
+		private void DrawBodyWalking(SpriteCompositionHelper helper, int frame, float cycleAngle)
 		{
-			Texture2D texture = GetTexture(Texture + "_Head");
-			float r = 0;
-			float bobAngle = IsWalking ? 2 * WalkCycleAngle : MathHelper.TwoPi * animationFrame / 90;
-			float bobAmplitude = IsWalking ? 2 : 1;
-			Vector2 pos = projectile.Center + Vector2.UnitX * projectile.spriteDirection * 10 + bobAmplitude * (float) Math.Sin(bobAngle) * Vector2.UnitY;
-			SpriteEffects effects = projectile.spriteDirection == 1 ? 0 : SpriteEffects.FlipHorizontally;
-			Rectangle bounds = new Rectangle(0, 0, texture.Width, texture.Height);
-			Vector2 origin = new Vector2(bounds.Width / 2, bounds.Height / 2);
-			spriteBatch.Draw(texture, pos - Main.screenPosition, bounds, lightColor, r, origin, 1, effects, 0);
+			float r = -MathHelper.Pi / 64 + MathHelper.Pi / 32 * (float)Math.Sin(cycleAngle);
+			// body
+			Vector2 offsetVector = (-2 + 2 * (float)Math.Sin(2 * cycleAngle)) * Vector2.UnitY;
+			helper.AddSpriteToBatch(GetTexture(Texture), offsetVector, r, 1);
+			// head
+			offsetVector = new Vector2(projectile.spriteDirection * 10, 2 * (float)Math.Sin(cycleAngle));
+			helper.AddSpriteToBatch(GetTexture(Texture + "_Head"), offsetVector);
+		}
+
+		private void DrawBodyIdle(SpriteCompositionHelper helper, int frame, float cycleAngle)
+		{
+			// body
+			Vector2 offsetVector = (-2 + (float)Math.Sin(cycleAngle + MathHelper.Pi / 2)) * Vector2.UnitY;
+			helper.AddSpriteToBatch(GetTexture(Texture), offsetVector);
+			
+			// head
+			offsetVector = new Vector2(projectile.spriteDirection * 10, (float)Math.Sin(cycleAngle));
+			scHelper.AddSpriteToBatch(GetTexture(Texture + "_Head"), offsetVector);
 		}
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			DrawLegs(spriteBatch, lightColor);
-			DrawBody(spriteBatch, lightColor);
-			DrawHead(spriteBatch, lightColor);
+			if(scHelper.IsWalking)
+			{
+				scHelper.Process(spriteBatch, lightColor, true, DrawLegs, DrawBodyWalking, DrawClouds);
+			} else
+			{
+				scHelper.Process(spriteBatch, lightColor, false, DrawLegs, DrawBodyIdle, DrawClouds);
+			}
 			return false;
 		}
 
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			if(IsBouncing)
+			{
+				bool hitFloor = (projectile.velocity.Y == 0 || projectile.velocity.Y == -0.5f) && oldVelocity.Y >= 0;
+				bool hitCeil = (projectile.velocity.Y == 0 || projectile.velocity.Y == 0.5f) && oldVelocity.Y < 0;
+				bool hitWall = projectile.velocity.X == 0;
+				if(hitFloor)
+				{
+					projectile.velocity.Y = Math.Min(-4, -projectile.oldVelocity.Y * 0.95f);
+				} 				
+				if(hitCeil)
+				{
+					projectile.velocity.Y = Math.Max(4, -projectile.oldVelocity.Y);
+				}
+				if(hitWall)
+				{
+					projectile.velocity.X = -oldVelocity.X;
+				}
+				return false;
+			} else
+			{
+				return base.OnTileCollide(oldVelocity);
+			}
+		}
 		public override void Animate(int minFrame = 0, int? maxFrame = null)
 		{
+			if(IsBouncing)
+			{
+				projectile.spriteDirection = Math.Sign(projectile.velocity.X);
+				projectile.rotation += projectile.spriteDirection * MathHelper.Pi / 16;
+				return;
+			} 				
 			projectile.rotation = 0;
 			if (vectorToTarget is Vector2 target && Math.Abs(target.X) < 1.5 * preferredDistanceFromTarget)
 			{
-				projectile.spriteDirection = -Math.Sign(target.X);
+				projectile.spriteDirection = Math.Sign(target.X);
 			} else if (projectile.velocity.X > 1)
 			{
 				projectile.spriteDirection = 1;
 			} else if (projectile.velocity.X < -1)
 			{
 				projectile.spriteDirection = -1;
-			}
-			if(Math.Abs(projectile.velocity.X) > 1)
-			{
-				walkCycleFrame++;
-			} else
-			{
-				walkCycleFrame = 0;
 			}
 		}
 	}
