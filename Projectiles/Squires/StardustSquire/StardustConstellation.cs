2a434b842629864896c59aeb867e803574d548c0
= <=>
2021-07-23 19:04:17 -0500

    more visual effects for stardust squire

--- Projectiles/Squires/StardustSquire/StardustConstellation.cs
+++ Projectiles/Squires/StardustSquire/StardustConstellation.cs
@@ -15,16 +15,22 @@ namespace AmuletOfManyMinions.Projectiles.Squires.StardustSquire
 	// This is an unfortunate class hierarchy
 	class StardustConstellation : TransientMinion
 	{
-		private SpriteCompositionHelper scHelper;
-		static int SpawnFrames = 15;
+		internal static int TimeToLive = 240;
 		internal static int ConstellationSize = 400;
 		internal static int BigStarCount = 16;
+		internal static int MaxSmallStars = 16;
+		internal static float SmallSpawnChance = 0.125f; // 25% chance to spawn a small star each frame
+
 		internal List<ConstellationStar> bigStars;
+		internal List<ConstellationSmallStar> smallStars;
+		private SpriteCompositionHelper scHelper;
+		private Texture2D smallStarTexture;
 
 		public override void SetDefaults()
 		{
 			base.SetDefaults();
-			projectile.timeLeft = 240;
+			projectile.timeLeft = TimeToLive;
+			smallStarTexture = ModContent.GetTexture(Texture + "_Small");
 			scHelper = new SpriteCompositionHelper(this, new Rectangle(0, 0, ConstellationSize, ConstellationSize))
 			{
 				idleCycleFrames = projectile.timeLeft,
@@ -37,6 +43,7 @@ namespace AmuletOfManyMinions.Projectiles.Squires.StardustSquire
 			base.OnSpawn();
 			scHelper.Attach();
 			SpawnBigStars();
+			smallStars = new List<ConstellationSmallStar>();
 		}
 
 		private void SpawnBigStars()
@@ -65,21 +72,89 @@ namespace AmuletOfManyMinions.Projectiles.Squires.StardustSquire
 			}
 		}
 
+		private void DrawSmallStars(SpriteCompositionHelper helper, int frame, float cycleAngle)
+		{
+			// could probably be accomplished with dust
+			for(int i = 0; i < smallStars.Count; i++)
+			{
+				smallStars[i].Draw(helper.spriteBatch, smallStarTexture, animationFrame);
+			}
+		}
+
 		public override void IdleMovement(Vector2 vectorToIdlePosition)
 		{
 			// no-op
 		}
 
+		private void UpdateSmallStars()
+		{
+			if (animationFrame < 15) 
+			{ 
+				return; 
+			}
+			int lastDeadIdx = -1;
+			for(int i = 0; i < smallStars.Count; i++)
+			{
+				if(smallStars[i].IsDead(animationFrame))
+				{
+					lastDeadIdx = i;
+				}
+			}
+			if(lastDeadIdx > -1)
+			{
+				// rather inefficient
+				smallStars = smallStars.Skip(lastDeadIdx + 1).ToList();
+			} 
+			if(smallStars.Count < MaxSmallStars && Main.rand.NextFloat() < SmallSpawnChance)
+			{
+				Vector2 parentPos = bigStars[Main.rand.Next(bigStars.Count)].EndOffset;
+				smallStars.Add(new ConstellationSmallStar(animationFrame, parentPos));
+			}
+		}
+
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			scHelper.Draw(spriteBatch, Color.White);
+			float fadeOutFrames = 30;
+			float brightness = projectile.timeLeft > fadeOutFrames ? 1 : projectile.timeLeft / fadeOutFrames;
+			scHelper.Draw(spriteBatch, Color.White * brightness);
 			return false;
 		}
 
 		public override void AfterMoving()
 		{
 			base.AfterMoving();
-			scHelper.UpdateDrawers(false, DrawBigStars);
+			UpdateSmallStars();
+			scHelper.UpdateDrawers(false, DrawSmallStars, DrawBigStars);
+		}
+	}
+
+	internal struct ConstellationSmallStar
+	{
+		static int TimeToLive = 60;
+
+		Vector2 position;
+		int spawnFrame;
+
+		internal bool IsDead(int frame) => frame - spawnFrame > TimeToLive;
+
+		public ConstellationSmallStar(int spawnFrame, Vector2 center)
+		{
+			int radius = 16 + Main.rand.Next(48);
+			position = center + Main.rand.NextFloat(MathHelper.TwoPi).ToRotationVector2() * radius;
+			this.spawnFrame = spawnFrame;
+		}
+
+		public void Draw(SpriteBatch spriteBatch, Texture2D texture, int frame)
+		{
+			int timeLeft = frame - spawnFrame;
+			if(timeLeft < 0)
+			{
+				return;
+			}
+			float brightness = 0.75f * (float)Math.Sin(MathHelper.Pi * timeLeft / TimeToLive);
+			spriteBatch.Draw(
+				texture, position, texture.Bounds, Color.White * brightness, 0, 
+				texture.Bounds.Center.ToVector2(), 0.5f, 0, 0);
 		}
 	}
 
@@ -89,12 +164,12 @@ namespace AmuletOfManyMinions.Projectiles.Squires.StardustSquire
 		static int AnimFrames = 60;
 		static int TierCount = 4; // number of distinct "rings" for stars to appear in
 		static int TierSize = StardustConstellation.BigStarCount / TierCount;
-		static int MaxRange = 3 * StardustConstellation.ConstellationSize / 8;
+		internal static int MaxRange = 3 * StardustConstellation.ConstellationSize / 8;
 		static int TierRadius = MaxRange / TierCount;
 		static int MaxConnection = 80;
 
 		Vector2 StartPoint;
-		Vector2 EndOffset;
+		internal Vector2 EndOffset;
 		Texture2D texture;
 		int idx;
 		int maxConnections;
