3f998cfc7d7fc8d1642f5a784ff2f82eb6c0635d
direwolf420 <amakejk1@web.de>
2021-09-27 00:41:37 +0200

    fix offset, remove commented code

--- DrawLayers/BaseRobeLayer.cs
+++ DrawLayers/BaseRobeLayer.cs
@@ -18,7 +18,6 @@ namespace AmuletOfManyMinions.DrawLayers
 		public override void Load()
 		{
 			//The equip tex will be assigned to the legs slot
-			//slotId = Mod.AddEquipTexture(null, EquipType.Legs, TexturePath);
 			Texture = ModContent.Request<Texture2D>(TexturePath);
 		}
 
@@ -47,29 +46,18 @@ namespace AmuletOfManyMinions.DrawLayers
 		{
 			Player drawPlayer = drawInfo.drawPlayer;
 
-			Color color = drawPlayer.GetImmuneAlphaPure(Lighting.GetColor((int)(drawPlayer.Center.X / 16), (int)(drawPlayer.Center.Y / 16)), drawInfo.shadow);
+			Color color = drawPlayer.GetImmuneAlphaPure(drawInfo.colorArmorBody, drawInfo.shadow);
 
 			Texture2D texture = Texture.Value;
 			Vector2 drawPos = drawInfo.Position;
-			drawPos.Y += 6;
 			drawPos += -Main.screenPosition + new Vector2(drawPlayer.width / 2 - drawPlayer.bodyFrame.Width / 2, drawPlayer.height - drawPlayer.bodyFrame.Height + 4f) + drawPlayer.bodyPosition;
-			//drawPos += new Vector2(drawPlayer.bodyFrame.Width / 2, drawPlayer.bodyFrame.Height / 2);
 			Vector2 legsOffset = drawInfo.legsOffset;
+
 			DrawData drawData = new DrawData(texture, drawPos.Floor() + legsOffset, drawPlayer.legFrame, color, drawPlayer.legRotation, legsOffset, 1f, drawInfo.playerEffect, 0)
 			{
 				shader = drawInfo.cBody
 			};
 			drawInfo.DrawDataCache.Add(drawData);
-
-			/*
-			Vector2 Position = drawInfo.position;
-			Position.Y += 14;
-			Color color = Lighting.GetColor((int)(drawPlayer.Center.X / 16), (int)(drawPlayer.Center.Y / 16));
-			Vector2 pos = new Vector2((float)((int)(Position.X - Main.screenPosition.X - (float)(drawPlayer.bodyFrame.Width / 2) + (float)(drawPlayer.width / 2))), (float)((int)(Position.Y - Main.screenPosition.Y + (float)drawPlayer.height - (float)drawPlayer.bodyFrame.Height + 4f))) + drawPlayer.bodyPosition + new Vector2((float)(drawPlayer.bodyFrame.Width / 2), (float)(drawPlayer.bodyFrame.Height / 2));
-			DrawData value = new DrawData(texture, pos, new Microsoft.Xna.Framework.Rectangle?(drawPlayer.legFrame), color, drawPlayer.legRotation, drawInfo.legOrigin, 1f, drawInfo.spriteEffects, 0);
-			value.shader = drawPlayer.cBody;
-			Main.playerDrawData.Add(value);
-			 */
 		}
 	}
 }
