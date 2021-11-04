1b99833483fb679e7191c8b3ceb02b71f35ee760
= <=>
2021-11-04 07:49:14 -0500

    add baby werewolf pet

---
+++ Projectiles/Minions/CombatPets/JourneysEndVanillaClonePets/BabyWerewolf.cs
@@ -0,0 +1,34 @@
+﻿using AmuletOfManyMinions.Projectiles.Minions.CombatPets.CombatPetBaseClasses;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+using AmuletOfManyMinions.Projectiles.Minions.CombatPets.MasterModeBossPets;
+using AmuletOfManyMinions.Projectiles.Squires.PumpkinSquire;
+using Terraria;
+
+namespace AmuletOfManyMinions.Projectiles.Minions.CombatPets.JourneysEndVanillaClonePets
+{
+	public class BabyWerewolfMinionBuff : CombatPetVanillaCloneBuff
+	{
+		public BabyWerewolfMinionBuff() : base(ProjectileType<BabyWerewolfMinion>()) { }
+		public override string VanillaBuffName => "BabyWerewolf";
+		public override int VanillaBuffId => BuffID.BabyWerewolf;
+	}
+
+	public class BabyWerewolfMinionItem : CombatPetMinionItem<BabyWerewolfMinionBuff, BabyWerewolfMinion>
+	{
+		internal override string VanillaItemName => "FullMoonSqueakyToy";
+		internal override int VanillaItemID => ItemID.FullMoonSqueakyToy;
+	}
+
+	public class BabyWerewolfMinion : CombatPetGroundedMeleeMinion
+	{
+		public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.BabyWerewolf;
+		internal override int BuffId => BuffType<BabyWerewolfMinionBuff>();
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+			ConfigureDrawBox(24, 30, -4, -12, -1);
+			ConfigureFrames(24, (0, 3), (5, 17), (4, 4), (18, 23));
+		}
+	}
+}
