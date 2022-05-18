62acf0901ba2dbbec17713a6d1d043e124e5b100
Ouroel <68769795+Ouroel@users.noreply.github.com>
2022-05-18 20:09:55 +0800

    Update CombatPetLevelPacket.cs

--- Core/Netcode/Packets/CombatPetLevelPacket.cs
+++ Core/Netcode/Packets/CombatPetLevelPacket.cs
@@ -1,4 +1,4 @@
-﻿using AmuletOfManyMinions.Projectiles.Minions.CombatPets;
+using AmuletOfManyMinions.Projectiles.Minions.CombatPets;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,30 +15,35 @@ namespace AmuletOfManyMinions.Core.Netcode.Packets
 		// pet level is bounded to (0, 255)
 		readonly byte petLevel;
 		readonly short petDamage;
+		readonly object[] petModdedData;
 
 		public CombatPetLevelPacket() { }
 
-		public CombatPetLevelPacket(Player player, byte petLevel, short petDamage) : base(player)
+		public CombatPetLevelPacket(Player player, byte petLevel, short petDamage, object[] petModdedData) : base(player)
 		{
 			this.petLevel = petLevel;
 			this.petDamage = petDamage;
+			this.petModdedData = petModdedData;
 		}
 
 		protected override void PostSend(BinaryWriter writer, Player player)
 		{
 			writer.Write(petLevel);
 			writer.Write(petDamage);
+			CrossMod.CombatPetSendCrossModData(writer, petModdedData);
 		}
 
 		protected override void PostReceive(BinaryReader reader, int sender, Player player)
 		{
 			byte newLevel = reader.ReadByte();
 			short petDamage = reader.ReadInt16();
+			object[] petModdedStats = CrossMod.CombatPetReceiveCrossModData(reader);
 			// there may be unintended consequences to setting damage to zero
-			player.GetModPlayer<LeveledCombatPetModPlayer>().UpdatePetLevel(newLevel, petDamage, fromSync: true);
+			LeveledCombatPetModPlayer modPlayer = player.GetModPlayer<LeveledCombatPetModPlayer>();
+			modPlayer.UpdatePetLevel(newLevel, petDamage, petModdedStats, fromSync: true);
 			if (Main.netMode == NetmodeID.Server)
 			{
-				new CombatPetLevelPacket(player, newLevel, petDamage).Send(from: sender);
+				new CombatPetLevelPacket(player, newLevel, petDamage, petModdedStats).Send(from: sender);
 			}
 		}
 	}
