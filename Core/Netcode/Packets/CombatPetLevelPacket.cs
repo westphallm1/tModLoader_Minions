ef311e0917ddeaa2dc5f561ea7be1ca10dff40ec
Ouroel <68769795+Ouroel@users.noreply.github.com>
2022-05-18 20:20:57 +0800

    Update CombatPetLevelPacket.cs

--- Core/Netcode/Packets/CombatPetLevelPacket.cs
+++ Core/Netcode/Packets/CombatPetLevelPacket.cs
@@ -16,20 +16,23 @@ namespace AmuletOfManyMinions.Core.Netcode.Packets
 		readonly byte petLevel;
 		readonly short petDamage;
 		readonly object[] petModdedData;
+		readonly int petEmblemItem;
 
 		public CombatPetLevelPacket() { }
 
-		public CombatPetLevelPacket(Player player, byte petLevel, short petDamage, object[] petModdedData) : base(player)
+		public CombatPetLevelPacket(Player player, byte petLevel, short petDamage, int petEmblemItem, object[] petModdedData) : base(player)
 		{
 			this.petLevel = petLevel;
 			this.petDamage = petDamage;
 			this.petModdedData = petModdedData;
+			this.petEmblemItem = petEmblemItem;
 		}
 
 		protected override void PostSend(BinaryWriter writer, Player player)
 		{
 			writer.Write(petLevel);
 			writer.Write(petDamage);
+			writer.Write7BitEncodedInt(petEmblemItem);
 			CrossMod.CombatPetSendCrossModData(writer, petModdedData);
 		}
 
@@ -37,13 +40,14 @@ namespace AmuletOfManyMinions.Core.Netcode.Packets
 		{
 			byte newLevel = reader.ReadByte();
 			short petDamage = reader.ReadInt16();
+			int petEmblemItem = reader.Read7BitEncodedInt();
 			object[] petModdedStats = CrossMod.CombatPetReceiveCrossModData(reader);
 			// there may be unintended consequences to setting damage to zero
 			LeveledCombatPetModPlayer modPlayer = player.GetModPlayer<LeveledCombatPetModPlayer>();
-			modPlayer.UpdatePetLevel(newLevel, petDamage, petModdedStats, fromSync: true);
+			modPlayer.UpdatePetLevel(newLevel, petDamage, petEmblemItem, petModdedStats, fromSync: true);
 			if (Main.netMode == NetmodeID.Server)
 			{
-				new CombatPetLevelPacket(player, newLevel, petDamage, petModdedStats).Send(from: sender);
+				new CombatPetLevelPacket(player, newLevel, petDamage, petEmblemItem, petModdedStats).Send(from: sender);
 			}
 		}
 	}
