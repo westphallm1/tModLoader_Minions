d63bfd112a5c3c35d9ae3cc277fd3d6f66ba544c
= <=>
2022-08-11 23:21:32 -0500

    Implement basic lifecycle manager for cross mod AI

---
+++ Core/Minions/CrossModAI/CrossModAIGlobalProjectile.cs
@@ -0,0 +1,78 @@
+﻿using AmuletOfManyMinions.Core.Minions.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AmuletOfManyMinions.Core.Minions.CrossModAI
+{
+
+	internal interface ICrossModSimpleMinion : ISimpleMinion
+	{
+		bool DoVanillaAI();
+		void PostAI();
+
+		// This is a little roundabout circular reference. Should still be garbage collected (maybe)
+		SimpleMinionBehavior Behavior { get; }
+
+	}
+
+	internal delegate ICrossModSimpleMinion CrossModAISupplier(Projectile projectile);
+
+	internal class CrossModAIGlobalProjectile : GlobalProjectile
+	{
+
+		// TODO it might make sense to use InstancePerEntity rather than manually managing these lifecycles
+		internal static Dictionary<int, CrossModAISupplier> CrossModAISuppliers;
+
+		internal static Dictionary<int, ICrossModSimpleMinion> CrossModAIs;
+
+		public override void Load()
+		{
+			CrossModAISuppliers = new();
+			CrossModAIs = new();
+		}
+
+		public override void Unload()
+		{
+			CrossModAISuppliers = null;
+			CrossModAIs = null;
+		}
+
+		public override void SetDefaults(Projectile projectile)
+		{
+			if(CrossModAISuppliers.TryGetValue(projectile.type, out var supplier))
+			{
+				CrossModAIs[projectile.whoAmI] = supplier.Invoke(projectile);
+			}
+		}
+
+		public override bool PreAI(Projectile projectile)
+		{
+			if(!CrossModAIs.TryGetValue(projectile.whoAmI, out var crossModMinion))
+			{
+				return true;
+			}
+			crossModMinion.Behavior.MainBehavior();
+			// This feels a little roundabout
+			return crossModMinion.DoVanillaAI();
+		}
+
+		public override void PostAI(Projectile projectile)
+		{
+			// TODO
+		}
+
+		public override void Kill(Projectile projectile, int timeLeft)
+		{
+			if(CrossModAIs.ContainsKey(projectile.whoAmI))
+			{
+				CrossModAIs.Remove(projectile.whoAmI);
+			}
+		}
+
+	}
+}
