861eaf03dc00d2095365fa1940bed165655892a2
= <=>
2022-10-10 23:26:10 -0500

    add hook to manually enforce mutual exclusivity of all buff types that spawn a cross mod pet

--- Core/Minions/CrossModAI/CrossModAIGlobalProjectile.cs
+++ Core/Minions/CrossModAI/CrossModAIGlobalProjectile.cs
@@ -110,4 +110,83 @@ namespace AmuletOfManyMinions.Core.Minions.CrossModAI
 			return CrossModAI?.IsActive ?? base.MinionContactDamage(projectile);
 		}
 	}
+
+	public class AoMMVersionMultiBuffWorkaround: ModSystem
+	{
+		// HashMap from buff type to list of other buff types that should not 
+		// be active at the same time
+		private static Dictionary<int, HashSet<int>> CrossModCombatPetMutuallyExclusiveBuffs;
+
+		public override void PostSetupRecipes()
+		{
+			// TODO is this the appropriate location for this hook?
+
+			// Find all items that spawn a cross-mod registered pet, and create a one-to-many 
+			// mapping of all the buffs that can spawn that pet
+			var projIdToBuffTypeMap = new Dictionary<int, HashSet<int>>();
+
+			CrossModCombatPetMutuallyExclusiveBuffs = ModContent.GetContent<ModItem>()
+				.Select(mItem => mItem.Item)
+				// enumerable of all items that spawn a cross-mod pet
+				.Where(item => 
+					CrossModAIGlobalProjectile.CrossModAISuppliers.ContainsKey(item.shoot) &&
+					Main.vanityPet[item.buffType])
+				// one to many map of a projectile type to all items that spawn it
+				.GroupBy(i => i.shoot)
+				// many to many map of a buff type to all other buff types that spawn the same pet
+				.SelectMany(group => group.Select(g => new
+				{
+					BuffType = g.buffType,
+					OtherBuffTypes = group.Select(g2 => g2.buffType).Where(buffType => buffType != g.buffType).ToHashSet()
+				}))
+				.ToDictionary(pair=>pair.BuffType, pair=>pair.OtherBuffTypes);
+		}
+
+		public override void Load()
+		{
+			On.Terraria.Player.FreeUpPetsAndMinions += Player_FreeUpPetsAndMinions;
+			On.Terraria.Player.AddBuff += Player_AddBuff;
+		}
+
+		public override void Unload()
+		{
+			CrossModCombatPetMutuallyExclusiveBuffs = null;
+		}
+
+		private void Player_AddBuff(On.Terraria.Player.orig_AddBuff orig, Player self, int type, int timeToAdd, bool quiet, bool foodHack)
+		{
+			orig.Invoke(self, type, timeToAdd, quiet, foodHack);
+			if(CrossModCombatPetMutuallyExclusiveBuffs.TryGetValue(type, out var otherTypes))
+			{
+				foreach(var otherType in otherTypes)
+				{
+					self.ClearBuff(otherType);
+				}
+			}
+		}
+
+		private void Player_FreeUpPetsAndMinions(On.Terraria.Player.orig_FreeUpPetsAndMinions orig, Player self, Item sItem)
+		{
+			// Hack to fix a rather tricky bug that arises from AoMM and non-AoMM versions of cross-mod pet buffs
+			// spawning the same projectile. By default, the vanilla pet-free-up code will remove any existing instances of the
+			// projectile spawned by either the main buff or AoMM version buff, but not remove the buff themselves. This
+			// will leave the player in a state where both pet spawning buffs are active, and no copies of the pet are active.
+			// Both copies of the buff will then spawn the pet on the next frame, leaving the player with a duplicate.
+			// To circumvent this, temporarily un-flag the item as shooting the pet if the player already has one copy
+			// of the pet active, so that the single active copy will not despawn in the first place.
+			// We would ideally want to remove one buff as soon as the other version becomes active, but this does not
+			// appear to be easily accomplished.
+			int origShoot = sItem.shoot;
+			
+			if(Main.vanityPet[sItem.buffType] && self.ownedProjectileCounts[origShoot] > 0 && 
+				CrossModAIGlobalProjectile.CrossModAISuppliers.ContainsKey(sItem.shoot))
+			{
+				sItem.shoot = ProjectileID.None;
+			}
+			orig.Invoke(self, sItem);
+			sItem.shoot = origShoot;
+		}
+
+
+	}
 }
